92e98ace7a77e542b3780601fa5a6c54e129df9e
flaviaadiniz <flaviaadiniz@gmail.com>
2024-04-11 22:28:02 -0300

    Refactor exercise 1

--- src/main/java/semanas3e4/recursao/Lista1.java
+++ src/main/java/semanas3e4/recursao/Lista1.java
@@ -10,24 +10,24 @@ public class Lista1 {
     }
 
 
-
-
     // Ex 1: Crie um método recursivo que imprime um array recebido por parâmetro de trás para frente
     public static void printArrayBackwards(char[] array) throws IllegalArgumentException {
         if (array == null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("O array não pode ser nulo.");
         }
         printArrayBackwardsPrivate(array, array.length - 1);
     }
 
     private static void printArrayBackwardsPrivate(char[] array, int i) {
-        if (i == 0) {
-            System.out.println(array[i]);
-        } else {
-            System.out.print(array[i] + " ");
-            i--;
-            printArrayBackwardsPrivate(array, i);
+        if (i >= 0) {
+            System.out.print(array[i]);
+            printArrayBackwardsPrivate(array, i - 1);
         }
+//        } else {
+//            System.out.print(array[i] + " ");
+//            i--;
+//            printArrayBackwardsPrivate(array, i);
+//        }
     }
 
 }

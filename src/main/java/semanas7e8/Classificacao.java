5e6c5c931c310bed6858ce29ab2e30e5f6accb6c
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-16 20:51:12 -0300

    Add selectionSort method and examples of all sorting algorithms in use

--- src/main/java/semanas7e8/Classificacao.java
+++ src/main/java/semanas7e8/Classificacao.java
@@ -1,5 +1,7 @@
 package src.main.java.semanas7e8;
 
+import java.util.Arrays;
+
 public class Classificacao {
 
     // * também chamado de ORDENAÇÃO, é o processo de organizar itens em ordem, segundo algum critério
@@ -12,14 +14,22 @@ public class Classificacao {
 
     public static void main(String[] args) {
 
+        int[] array1 = {4, -3, 18, 22, 5, 0, 1, 47, -2, 9};
+
+        bubbleSort(array1);
+        insertionSort(array1);
+        selectionSort(array1);
+
     }
 
 
     // * Inserção Direta: divide o array em 2 segmentos: o primeiro com os elementos já ordenados, o segundo com os
     //   elementos ainda não ordenados
     // * pega o primeiro elemento do segmento não ordenado e procura seu lugar no segmento ordenado
+    // ** ESTÁVEL
 
     public static void insertionSort(int[] array) {
+        System.out.println(Arrays.toString(array));
         for (int i = 1; i < array.length; i ++) {
             int j = i; // posição do primeiro elemento no segmento não ordenado
             int B = array[i]; // primeiro elemento no segmento não ordenado
@@ -29,16 +39,18 @@ public class Classificacao {
                 array[j] = array[j-1];
                 j--;
             }
-
             array[j] = B;
+            System.out.println(Arrays.toString(array));
         }
     }
 
 
     // Bubblesort (Método da bolha): compara sempre de 2 em 2 e coloca o de valor menor na 1a posição entre as duas
     // * é necessário mais de uma varredura - na primeira varredura, o maior valor sempre fica por último!
+    // ** ESTÁVEL
 
     public static void bubbleSort(int[] array) {
+        System.out.println(Arrays.toString(array));
         int i = array.length - 1;
 
         while (i > 0) {
@@ -51,9 +63,35 @@ public class Classificacao {
                     lastFlipped = j;
                 }
             }
+            System.out.println(Arrays.toString(array));
             i = lastFlipped;
         }
     }
 
+
+    // Seleção Direta: a seleção do menor elemento é feita por pesquisa sequencial
+    // - o menor elemento encontrado é permutado com o que ocupa a posição inicial do array
+    // - o processo de seleção é repetido para a parte restante do array, até que todos os elementos tenham sido selecionados
+    //   e colocados em suas posições definitivas
+    // ** NÃO ESTÁVEL
+
+
+    public static void selectionSort(int[] array) {
+        System.out.println(Arrays.toString(array));
+        int min = 0;
+
+        for (int i = 0; i <array.length - 1; i++) {
+            min = i; // mínimo inicial
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j; // acha o novo mínimo
+                }
+            }
+            int T = array[i]; // coloca o novo mínimo (min) na posição correta (i)
+            array[i] = array[min];
+            array[min] = T;
+            System.out.println(Arrays.toString(array));
+        }
+    }
 
 }

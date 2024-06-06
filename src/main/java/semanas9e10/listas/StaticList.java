b4edd03480e6f3390a6d50e24180f8fe7d7294da
flaviaadiniz <flaviaadiniz@gmail.com>
2024-06-05 21:14:48 -0300

    Implement methods insert and remove; add a test class.

--- src/main/java/semanas9e10/listas/StaticList.java
+++ src/main/java/semanas9e10/listas/StaticList.java
@@ -1,5 +1,8 @@
 package src.main.java.semanas9e10.listas;
 
+import java.nio.BufferUnderflowException;
+import java.util.Arrays;
+
 public class StaticList<E> implements List<E> {
 
     protected E[] elements;
@@ -26,13 +29,49 @@ public class StaticList<E> implements List<E> {
     }
 
     @Override
-    public void insert(E element, int position) {
+    public void insert(E element, int position) throws StackOverflowError, IndexOutOfBoundsException {
 
+        // verifica se há espaço na lista
+        if (isFull()) {
+            throw new StackOverflowError();
+        }
+
+        // verifica se a posição é válida
+        if (position < 0 || position > numElements) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        // desloca para a direita os elementos necessários, abrindo espaço para o novo
+        for (int i = numElements - 1; i >= position; i--) {
+            elements[i+1] = elements[i];
+        }
+
+        // armazena o novo elemento e ajusta o total
+        elements[position] = element;
+        numElements++;
     }
 
     @Override
-    public E remove(int position) {
-        return null;
+    public E remove(int position) throws BufferUnderflowException, IndexOutOfBoundsException {
+
+        // verifica se a lista está vazia
+        if (isEmpty()) {
+            throw new BufferUnderflowException();
+        }
+
+        // guarda uma referência temporária ao elemento removido
+        E element = elements[position];
+
+        // desloca para a esquerda os elementos necessários, sobrescrevendo a posição do que está sendo removido
+        for (int i = position; i < numElements - 1; i++) {
+            elements[i] = elements[i+1];
+        }
+
+        // define para null a posição antes ocupada pelo último, para que a coleta de lixo possa atuar e ajusta o total
+        elements[numElements - 1] = null;
+        numElements--;
+
+        return element;
     }
 
     @Override
@@ -43,5 +82,12 @@ public class StaticList<E> implements List<E> {
     @Override
     public int search(E element) {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "StaticList{" +
+                "elements=" + Arrays.toString(elements) +
+                '}';
     }
 }
---
+++ src/main/java/semanas9e10/listas/StaticListTest.java
@@ -0,0 +1,26 @@
+package src.main.java.semanas9e10.listas;
+
+import java.util.Arrays;
+
+public class StaticListTest {
+
+    public static void main(String[] args) {
+
+        List<Integer> lista = new StaticList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            lista.insert(i*2, i);
+        }
+
+
+        System.out.println("Lista após inserções: ");
+        System.out.println(lista);
+
+        System.out.println(lista.remove(5) + " foi removido da lista!");
+
+        System.out.println("Lista após remoção do elemento na posição 5: ");
+        System.out.println(lista);
+
+    }
+
+}

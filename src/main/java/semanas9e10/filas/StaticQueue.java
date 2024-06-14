76080e9dbc31f5e35df0bd7594ff14aa4a031bbd
flaviaadiniz <flaviaadiniz@gmail.com>
2024-06-13 21:16:43 -0300

    Implement methods on StaticQueue

--- src/main/java/semanas9e10/filas/StaticQueue.java
+++ src/main/java/semanas9e10/filas/StaticQueue.java
@@ -4,53 +4,84 @@ import src.main.java.semanas9e10.classeslab2.OverflowException;
 import src.main.java.semanas9e10.classeslab2.UnderflowException;
 
 public class StaticQueue<E> implements Queue<E> {
+    private int first;
 
+    private int last;
 
-    protected int first;
-
-    protected int last;
-
-    protected E elements[];
-
+    private E elements[];
 
     public StaticQueue(int maxSize) {
-        elements = (E[]) new Object[maxSize];
-        first = last =-1;
+        elements = (E[])new Object[maxSize];
+        first = last = -1;
     }
 
-
-    @Override
-    public int numElements() {
-        return 0;
-    }
-
-    @Override
     public boolean isEmpty() {
-        return false;
+        return first == -1;
     }
 
-    @Override
     public boolean isFull() {
-        return false;
+        return first == ((last + 1) % elements.length);
     }
 
-    @Override
-    public E front() throws UnderflowException {
-        return null;
+    public int numElements() {
+        if (isEmpty())
+            return 0;
+        else {
+            int n = elements.length; // para legibilidade da expressão abaixo
+            return ((n + last - first) % n) + 1;
+        }
     }
 
-    @Override
-    public E back() throws UnderflowException {
-        return null;
-    }
-
-    @Override
     public void enqueue(E element) throws OverflowException {
+        if (isFull())
+            throw new OverflowException();
+        else {
+            if (last == -1)
+                first = last = 0;
+            else
+                last = (last + 1) % elements.length;
+            elements[last] = element;
+        }
 
     }
 
-    @Override
     public E dequeue() throws UnderflowException {
-        return null;
+        if (isEmpty())
+            throw new UnderflowException();
+        E element = elements[first];
+        elements[first] = null; // para coleta de lixo
+        if (first == last)
+            first = last = -1;
+        else
+            first = (first + 1) % elements.length;
+
+        return element;
+    }
+
+    public E front() throws UnderflowException {
+        if (isEmpty())
+            throw new UnderflowException();
+        return elements[first];
+    }
+
+    public E back() throws UnderflowException {
+        if (isEmpty())
+            throw new UnderflowException();
+        return elements[last];
+    }
+
+    public String toString() {
+        if (isEmpty())
+            return "[Empty]";
+        else {
+            String s = "[" + elements[first];
+            int n = numElements();
+            for (int i = 1; i < n; i++) {
+                int k = (first + i) % elements.length;
+                s += ", " + elements[k];
+            }
+            s += "]";
+            return s;
+        }
     }
 }
